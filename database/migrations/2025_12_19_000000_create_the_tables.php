0d706290305aab5adc3cf162b11cdc7c8c5a19be
Ron Northrip <ron.northrip@gmail.com>
2025-12-19 14:36:19 -0500

    Base set of models and a migration

---
+++ database/migrations/2025_12_19_000000_create_the_tables.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+        Schema::dropIfExists('animals');
+        Schema::dropIfExists('countries');
+        Schema::dropIfExists('locations');
+        Schema::dropIfExists('people');
+    }
+};
