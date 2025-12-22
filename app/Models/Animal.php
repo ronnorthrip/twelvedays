16e97a37fe95c5f42368db887dc13ee5a55dae37
Ron Northrip <ron.northrip@gmail.com>
2025-12-22 13:15:10 -0500

    Prefil the trait too

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Models;
-
+use App\Traits\CanBeSung;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Animal extends Model
 {
