fa53ed2a5902bb251ac84554281df5cff53cd504
Ron Northrip <ron.northrip@gmail.com>
2025-12-22 07:19:22 -0500

    Song comment in db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // A partridge in a pear tree!
+        // Two turtle doves,
+        // Three French hens,
+        // Four calling birds,
+        // Five golden rings,
+        // Six geese a-laying,
+        // Seven swans a-swimming,
+        // Eight maids a-milking,
+        // Nine ladies dancing,
+        // Ten lords a-leaping,
+        // Eleven pipers piping,
+        // Twelve drummers drumming,
     }
 }
