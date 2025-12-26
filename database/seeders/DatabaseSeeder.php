77d3d115346d4f5a6edf4d96477f81224fdd3bae
Ron Northrip <ron.northrip@gmail.com>
2025-12-26 14:56:56 -0500

    Seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,16 +23,91 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // A partridge in a pear tree!
+        $day = \App\Models\Day::create(['name'=>'First']);
+        $animal = \App\Models\Animal::create(['name' => 'Partidge']);
+        $location = \App\Models\Location::create(['name' => 'in a pear tree']);
+        $lyric1 = $day->lyrics()->make();
+        $lyric2 = $day->lyrics()->make();
+        $lyric1->singable()->associate($animal)->save();
+        $lyric2->singable()->associate($location)->save();
         // Two turtle doves,
-        // Three French hens,
+        $day = \App\Models\Day::create(['name'=>'Second']);
+        $animal = \App\Models\Animal::create(['name' => 'turtle doves']);
+        $lyric1 = $day->lyrics()->make();
+        $lyric1->singable()->associate($animal)->save();
+        // Three french hens,
+        $day = \App\Models\Day::create(['name'=>'Third']);
+        $location = \App\Models\Location::create(['name' => 'french']);
+        $animal = \App\Models\Animal::create(['name' => 'hens']);
+        $lyric1 = $day->lyrics()->make();
+        $lyric2 = $day->lyrics()->make();
+        $lyric1->singable()->associate($location)->save();
+        $lyric2->singable()->associate($animal)->save();
         // Four calling birds,
+        $day = \App\Models\Day::create(['name'=>'Fourth']);
+        $animal = \App\Models\Animal::create(['name' => 'turtle doves']);
+        $lyric1 = $day->lyrics()->make();
+        $lyric1->singable()->associate($animal)->save();
         // Five golden rings,
+        $day = \App\Models\Day::create(['name'=>'Fifth']);
+        $jewelry = \App\Models\Jewelry::create(['name' => 'golden rings']);
+        $lyric1 = $day->lyrics()->make();
+        $lyric1->singable()->associate($jewelry)->save();
         // Six geese a-laying,
+        $day = \App\Models\Day::create(['name'=>'Sixth']);
+        $animal = \App\Models\Animal::create(['name' => 'geese']);
+        $activity = \App\Models\Activity::create(['name' => 'a-laying']);
+        $lyric1 = $day->lyrics()->make();
+        $lyric2 = $day->lyrics()->make();
+        $lyric1->singable()->associate($animal)->save();
+        $lyric2->singable()->associate($activity)->save();
         // Seven swans a-swimming,
+        $day = \App\Models\Day::create(['name'=>'Seventh']);
+        $animal = \App\Models\Animal::create(['name' => 'swans']);
+        $activity = \App\Models\Activity::create(['name' => 'a-swimming']);
+        $lyric1 = $day->lyrics()->make();
+        $lyric2 = $day->lyrics()->make();
+        $lyric1->singable()->associate($animal)->save();
+        $lyric2->singable()->associate($activity)->save();
         // Eight maids a-milking,
+        $day = \App\Models\Day::create(['name'=>'Eighth']);
+        $animal = \App\Models\Animal::create(['name' => 'maids']);
+        $activity = \App\Models\Activity::create(['name' => 'a-milking']);
+        $lyric1 = $day->lyrics()->make();
+        $lyric2 = $day->lyrics()->make();
+        $lyric1->singable()->associate($animal)->save();
+        $lyric2->singable()->associate($activity)->save();
         // Nine ladies dancing,
+        $day = \App\Models\Day::create(['name'=>'Ninth']);
+        $person = \App\Models\Person::create(['name' => 'ladies']);
+        $activity = \App\Models\Activity::create(['name' => 'dancing']);
+        $lyric1 = $day->lyrics()->make();
+        $lyric2 = $day->lyrics()->make();
+        $lyric1->singable()->associate($person)->save();
+        $lyric2->singable()->associate($activity)->save();
         // Ten lords a-leaping,
+        $day = \App\Models\Day::create(['name'=>'Tenth']);
+        $person = \App\Models\Person::create(['name' => 'lords']);
+        $activity = \App\Models\Activity::create(['name' => 'a-leaping']);
+        $lyric1 = $day->lyrics()->make();
+        $lyric2 = $day->lyrics()->make();
+        $lyric1->singable()->associate($person)->save();
+        $lyric2->singable()->associate($activity)->save();
         // Eleven pipers piping,
+        $day = \App\Models\Day::create(['name'=>'Eleventh']);
+        $person = \App\Models\Person::create(['name' => 'pipers']);
+        $activity = \App\Models\Activity::create(['name' => 'piping']);
+        $lyric1 = $day->lyrics()->make();
+        $lyric2 = $day->lyrics()->make();
+        $lyric1->singable()->associate($person)->save();
+        $lyric2->singable()->associate($activity)->save();
         // Twelve drummers drumming,
+        $day = \App\Models\Day::create(['name'=>'Twelveth']);
+        $person = \App\Models\Person::create(['name' => 'drummers']);
+        $activity = \App\Models\Activity::create(['name' => 'drumming']);
+        $lyric1 = $day->lyrics()->make();
+        $lyric2 = $day->lyrics()->make();
+        $lyric1->singable()->associate($person)->save();
+        $lyric2->singable()->associate($activity)->save();
     }
 }
